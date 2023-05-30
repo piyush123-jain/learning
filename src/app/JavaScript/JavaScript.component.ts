8ecec15ff26e4346f9603d3ca9e1901d8472f1f0
piyush jain <piyushj1936@gmail.com>
2023-05-30 14:57:56 +0530

    app Javascript component

---
+++ src/app/JavaScript/JavaScript.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-JavaScript',
+  templateUrl: './JavaScript.component.html',
+  styleUrls: ['./JavaScript.component.css']
+})
+export class JavaScriptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JavaScriptComponent } from './JavaScript/JavaScript.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      JavaScriptComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
