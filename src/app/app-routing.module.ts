d0fc3cb8f50772badde4a29f597fc782477aeee7
piyushj1998 <piyushj1998@gmail.com>
2024-03-07 09:37:33 +0530

    Javascript learning

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { JavaScriptComponent } from './JavaScript/JavaScript.component';
+import { HomeComponent } from './home/home.component';
+import { AngularComponent } from './Angular/Angular.component';
+import { InterviewQuestionsComponent } from './Interview/interview-questions.component';
+
+// { path: '', component: FirstComponent },
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+  path:"javascript",
+  component:JavaScriptComponent
+  },
+  {
+  path:"angular",
+  component:AngularComponent
+  },
+  {
+  path:"questions",
+  component:InterviewQuestionsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64] })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
